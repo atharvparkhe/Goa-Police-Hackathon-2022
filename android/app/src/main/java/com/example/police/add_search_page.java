24ed5ae5d11aa56f2534abb3cf4e0d135308dd20
ruth <ruthveekdessai04@gmail.com>
2022-09-10 10:46:04 +0530

    added android login page

---
+++ android/app/src/main/java/com/example/police/add_search_page.java
@@ -0,0 +1,14 @@
+package com.example.police;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_search_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_search_page);
+    }
+}
---
+++ android/app/src/main/java/com/example/police/firstpage.java
@@ -0,0 +1,14 @@
+package com.example.police;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class firstpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstpage);
+    }
+}
